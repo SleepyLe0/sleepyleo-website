e5db28caac7e6ae36d23a10429a2fa057f858873
SleepyLe0 <kunkundids@gmail.com>
2026-02-04 20:37:20 +0700

    chore: explicitly pass database URL to prisma db push command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-bunx prisma db push --accept-data-loss
+bunx prisma db push --url "$DATABASE_URL" --accept-data-loss
 
 echo "Starting server..."
 exec bun server.js
